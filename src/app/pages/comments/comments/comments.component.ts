ab4b4941237acd4641602f27530465f84a6b28ab
Veljko <veljkomiljkovic@live.com>
2020-05-22 11:59:12 +0200

    Load more button fixed. First level comments done.

--- src/app/pages/comments/comments/comments.component.ts
+++ src/app/pages/comments/comments/comments.component.ts
@@ -1,8 +1,10 @@
+import { getComment } from './../../../store/actions/top-stories.actions';
 import { AppState } from './../../../store/models/app-state.model';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { commentItem } from 'src/app/store/models/story-item.model';
 
 @Component({
   selector: 'app-comments',
@@ -11,17 +13,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CommentsComponent implements OnInit {
   id: number;
-  comments$: Observable<number[]> = this.store.select(
-    store => store.state.storyItems.find(c => c.id == this.id).kids
+  comments$: Observable<commentItem[]> = this.store.select(
+    store => store.state.comments
   );
 
   constructor(
     private route: ActivatedRoute,
     private store: Store<AppState>
-    ) { }
+  ) {
+    this.id = this.route.snapshot.params['id'];
+    this.store.dispatch(getComment({ id: this.id }))
+   }
 
   ngOnInit(): void {
-    this.id = this.route.snapshot.params['id'];
+
   }
 
 }
--- src/app/store/models/story-item.model.ts
+++ src/app/store/models/story-item.model.ts
@@ -2,20 +2,22 @@ export interface storyItem {
   by: string,
   descendants: number,
   id: number,
-  kids?: number[],
+  kids: number[],
+  comments: commentItem[],
   score: number,
   time: number,
   title: string,
   type: string,
-  url: string
+  url: string,
 }
 
 export interface commentItem {
   by: string,
   id: number,
-  kids?: number[],
+  kids: number[],
+  comments: commentItem[],
   parent: number,
   text: string,
   time: number,
-  type: string
+  type: string,
 }
